62afb655e92cac19f0c0e349855c0ba5a0b236d7
vasanth <vasanth@drylogics.com>
2022-08-19 16:29:10 +0530

    added tests to binary search

--- src/binarysearch_recursion.rb
+++ src/binarysearch_recursion.rb
@@ -1,4 +1,3 @@
-arr = [5, 7, 11, 13, 15, 17, 21, 23, 25, 30]
 def binary_search(arr, num, initial=0, last=arr.length-1)
   if initial > last
     return -1
@@ -14,4 +13,28 @@ def binary_search(arr, num, initial=0, last=arr.length-1)
     binary_search(arr, num, initial, last)
   end
 end
-puts binary_search(arr,30)
+
+arr = [5, 7, 11, 13, 15, 16, 21, 23, 25, 30]
+
+arr.each_with_index do |n, i|
+  if(i != binary_search(arr, n))
+    raise "Error: Failed to correctly find #{n}"
+  end
+end
+
+# out of range
+[4, 32].each do |n|
+  if(-1 != binary_search(arr, n))
+    raise "Error: Did not return -1 for number #{n}"
+  end
+end
+
+
+# In Range, non-existent
+[17, 8, 22, 27].each do |n|
+  if(-1 != binary_search(arr, n))
+    raise "Error: Did not return -1 for number #{n}"
+  end
+end
+
+puts "Binary Search Works!!!"
