b5bd0f7dbad4c1840093b54eacc39e5696f43c20
Danish Benson <danibenz17@gmail.com>
2022-08-23 11:41:59 +0530

    upto binary search

--- src/binarysearch_recursion.rb
+++ src/binarysearch_recursion.rb
@@ -1,72 +1,124 @@
-def binary_search(students, name, initial = 0, last = students.length - 1)
-  if initial > last
-    return -1
-  end
-  mid = (initial + last) / 2
-  case name <=> students[mid]
-  when 1
-    initial = mid + 1
-    binary_search(students, name, initial, last)
-  when -1
-    last = mid - 1
-    binary_search(students, name, initial, last)
-  else
+a = [10, 11, 13, 14, 17, 21, 23, 27, 29, 30]
+def binary_search(objects, search_obj, low = 0, high = objects.length - 1)
+  mid = ( high - low + 1 )/2 + low
+  return -1 if low > high
+  case objects[mid] <=> search_obj
+  when 0
     return mid
+  when -1
+    low = mid + 1 
+    return binary_search(objects, search_obj, low, high)
+  else
+    high = mid - 1
+    return binary_search(objects, search_obj, low, high)
   end
-end
+end 
 
-arr = [5, 7, 11, 13, 15, 16, 21, 23, 25, 30]
+arr = [10, 11, 13, 14, 17, 21, 23, 27, 29, 30]
 
 arr.each_with_index do |n, i|
   if(i != binary_search(arr, n))
-    raise "Error: Failed to correctly find #{n}"
+   raise "Error: Failed to correctly find #{n}"
   end
 end
-
+  
 # out of range
 [4, 32].each do |n|
   if(-1 != binary_search(arr, n))
     raise "Error: Did not return -1 for number #{n}"
   end
 end
-
-
+  
+  
 # In Range, non-existent
-[17, 8, 22, 27].each do |n|
+[1, 8, 22, 2].each do |n|
   if(-1 != binary_search(arr, n))
     raise "Error: Did not return -1 for number #{n}"
   end
 end
 
-puts "Binary Search Works For Numbers!!!"
+puts "Binary Search Works for numbers!!!"
 
-students = ["abi", "anish", "benson", "jack", "karthik", "rathish", "sam", "shree" "varun", "varshini"]
 
-students.each_with_index do |name, i|
-  if(i != binary_search(students, name))
-    raise "Error: Failed to correctly find #{name}"
+
+names = ["abi", "anish", "benson", "jack", "karthik",
+           "rathish", "sam", "shree" "varun", "varshini"]
+
+names.each_with_index do |n, i|
+  if(i != binary_search(names, n))
+    raise "Error: Failed to correctly find #{n}"
   end
 end
-
-students.each_with_index do |name, i|
-  if(i != binary_search(students, name))
-    raise "Error: Failed to correctly find #{name}"
-  end
-end
-
+  
 # out of range
-['aarthi', 'zahir'].each do |name|
-  if(-1 != binary_search(students, name))
-    raise "Error: Did not return -1 for number #{name}"
+['aarthi', 'zahir'].each do |n|
+  if(-1 != binary_search(names, n))
+    raise "Error: Did not return -1 for number #{n}"
   end
 end
-
-
+  
+  
 # In Range, non-existent
-['sathish', 'hari'].each do |name|
-  if(-1 != binary_search(students, name))
-    raise "Error: Did not return -1 for number #{name}"
+['sathish', 'hari'].each do |n|
+  if(-1 != binary_search(names, n))
+    raise "Error: Did not return -1 for number #{n}"
   end
 end
 
-puts "Binary Search Works For Strings!!!"
+   
+puts "Binary Search Works for strings!!!"
+
+class Student
+  attr_reader :name, :age, :dob, :rollno, :pincode
+  def initialize(rollno, name, age, dob, pincode)
+    @rollno = rollno
+    @name = name
+    @dob = dob
+    @age = age
+    @pincode = pincode
+  end
+
+  def <=> (rhs)
+    self.rollno <=> rhs.rollno
+  end
+end 
+
+students_data = [
+  [1, "Ann", 19, "19/09/2003", 643112], 
+  [2, "Ben", 22, "02/05/2000", 643551], 
+  [3, "Dani", 21, "06/03/2001", 643155], 
+  [5, "Deb", 24, "07/07/1998", 643142], 
+  [6, "Jo", 21, "19/09/2001", 643889], 
+  [7, "Jeny", 25, "04/11/1997", 643209],
+  [9, "Peter", 24, "10/11/1998", 641037],
+  [10, "Paul", 22, "19/09/2000", 641077],
+  [11, "Sam", 20, "25/12/2002", 641097],
+]
+students = students_data.map { |st|
+  Student.new(st[0], st[1], st[2], st[3], st[4])
+}
+
+students.each_with_index do |student, i|
+  if(i != binary_search(students, student))
+    raise "Error: Failed to correctly find #{student}"
+  end
+end
+  
+# out of range
+[0, 32].each do |student|
+  student = Student.new(0, "spider", 22, "10/12/12", 641223)
+  if(-1 != binary_search(students, student))
+    raise "Error: Did not return -1 for number #{student}"
+  end
+end
+  
+  
+# In Range, non-existent
+[4, 8].each do |student|
+  student = Student.new(4, "mark", 23, "10/12/1998", 641276)
+  if(-1 != binary_search(students, student))
+    raise "Error: Did not return -1 for number #{student}"
+  end
+end
+
+puts "Binary Search Works for students!!!"
--- src/class_students.rb
+++ src/class_students.rb
@@ -1,18 +1,20 @@
 class Students
-    def initialize(name ,age ,dob, rollno, pincode)
+    attr_accessor :name, :age, :dob, :rollno, :pincode
+  def initialize(name ,age ,dob, rollno, pincode)
     @name = name
-    @age = age
+    @age = age 
     @dob = dob
     @rollno = rollno
     @pincode = pincode
-    end
-    def display_data()
-    puts @name, @age, @dob, @rollno, @pincode
-    end
+  end
+    # def display_data()
+    # puts @name, @age, @dob, @rollno, @pincode
+    # end
 end
 student1 = Students.new("Danish", 21, "01-03-2001", 19106001, 641037)
 student2 = Students.new("Rathish", 23, "10-07-1997", 19106002, 641045 )
 student3 = Students.new("sathish", 24, "12-10-1996", 19106003, 641001)
-student1.display_data()
-student2.display_data()
-student3.display_data()
+puts "#{student1.rollno}, My Name is #{student1.name} and I am #{student1.age} and DOB 
+is #{student1.dob} and living in the pincode #{student1.pincode}"
+# student2.display_data()
+# student3.display_data()
